1a0e1db9731dd0587fe6a92fd4fac7b659a4e21d
Martin Craig <martin.craig@nottingham.ac.uk>
2021-09-17 11:52:54 +0100

    Add defaced suffix to output

--- docker/deface_wrapper.sh
+++ docker/deface_wrapper.sh
@@ -6,6 +6,6 @@ OUTDIR=$2
 shopt -s nullglob
 for FILE in "${INDIR}"/*.{nii,nii.gz}; do
   FNAME=`basename $FILE`
-  OUTFILE=${FNAME%%.*}
+  OUTFILE=${FNAME%%.*}_defaced
   fsl_deface $FILE $OUTDIR/$OUTFILE
 done
