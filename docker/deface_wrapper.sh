84b07e24686086bb4cd172d09724445678791bde
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-09-24 10:39:54 +0100

    Add argument to filter files

--- docker/deface_wrapper.sh
+++ docker/deface_wrapper.sh
@@ -2,9 +2,14 @@
 
 INDIR=$1
 OUTDIR=$2
+FILTER=$3
+
+if [ -z $FILTER ]; then
+  FILTER="*"
+fi
 
 shopt -s nullglob
-for FILE in "${INDIR}"/*.{nii,nii.gz}; do
+for FILE in "${INDIR}"/$FILTER.{nii,nii.gz}; do
   FNAME=`basename $FILE`
   OUTFILE=${FNAME%%.*}_defaced
   fsl_deface $FILE $OUTDIR/$OUTFILE
